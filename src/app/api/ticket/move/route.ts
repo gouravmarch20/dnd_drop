e2164a5812ed7213309a5bf7041ac78eab8f22ed
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-09-24 08:55:57 +0530

    api flow

---
+++ src/app/api/ticket/move/route.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+import { schema } from "@/schema/moveTicket";
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const response = schema.safeParse(body);
+    if (!response.success) {
+      return Response.json(
+        { error: { message: "Fields are missing or passed incorrect validation failed -" } },
+        { status: 400 }
+      );
+    }
+    const { boardId, position, boardColumnId, ticketId } = body;
+    console.log("debug_", boardId, position, boardColumnId);
+
+    const ticketData = await prisma.boardTicket.update({
+      where: {
+        id: ticketId,
+      },
+      data: {
+        boardId,
+        position,
+        boardColumnId,
+      },
+    });
+
+    return Response.json(
+      {
+        data: {
+          ticketData,
+        },
+      },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.log(e);
+    return Response.json(
+      { error: { message: "Something went wrong!" } },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/schema/moveTicket.ts
@@ -0,0 +1,9 @@
+
+import { z } from "zod";
+
+export const schema = z.object({
+  boardId: z.string(),
+  position: z.number(),
+  boardColumnId: z.string(),
+  ticketId: z.string(),
+});
