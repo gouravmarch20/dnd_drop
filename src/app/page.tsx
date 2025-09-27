f1f3edb270aef3f0228f1585f2f227bd19959f9a
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-09-27 17:51:44 +0530

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import Image from "next/image";
 import A from "@/components/A";
-
+import { Button } from "@/stories/Button";
 export default function Home() {
   return (
     <div className="">
+      <Button label="dix" />
       <A />
     </div>
   );
